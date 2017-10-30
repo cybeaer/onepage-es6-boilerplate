dbcf46bbd29fe1d74d46b55f151a1c04c58425b1
Michael Attenberger <attenberger@gmail.com>
2017-10-31 00:08:21 +0100

    basic babelify action by gulp

---
+++ gulpfile.js
@@ -0,0 +1,145 @@
+var del = require('del');
+var gulp = require('gulp');
+var path = require('path');
+var argv = require('yargs').argv;
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var buffer = require('gulp-buffer');
+var uglify = require('gulp-uglify');
+var gulpif = require('gulp-if');
+var exorcist = require('exorcist');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var browserSync = require('browser-sync');
+
+/** Foldner names **/
+var BUILD_PATH = './dist';
+var SCRIPTS_PATH = BUILD_PATH + '/scripts';
+var SOURCE_PATH = './src';
+var STATIC_PATH = './static';
+var ENTRY_FILE = SOURCE_PATH + '/index.js';
+var OUTPUT_FILE = 'app.js';
+
+var keepFiles = false;
+
+/**
+ * is it productive?
+ */
+function isProduction() {
+    return argv.production;
+}
+
+/**
+ * outputs if its running in dev or dist mode
+ */
+function logBuildMode() {
+    if (isProduction()) {
+        gutil.log(gutil.colors.green('Running production build...'));
+    } else {
+        gutil.log(gutil.colors.yellow('Running development build...'));
+    }
+}
+
+/**
+ * Deletes all content inside the './build' folder.
+ * If 'keepFiles' is true, no files will be deleted. This is a dirty workaround since we can't have
+ * optional task dependencies :(
+ * Note: keepFiles is set to true by gulp.watch (see serve()) and reseted here to avoid conflicts.
+ */
+function cleanBuild() {
+    if (!keepFiles) {
+        del(['dist/**/*.*']);
+    } else {
+        keepFiles = false;
+    }
+}
+
+/**
+ * Copies the content of the './static' folder into the '/dist' folder.
+ */
+function copyStatic() {
+    return gulp.src(STATIC_PATH + '/**/*')
+        .pipe(gulp.dest(BUILD_PATH));
+}
+
+/**
+ * Transforms ES2015 code into ES5 code.
+ * Optionally: Creates a sourcemap file 'game.js.map' for debugging.
+ *
+ * In order to avoid copying Phaser and Static files on each build,
+ * I've abstracted the build logic into a separate function. This way
+ * two different tasks (build and fastBuild) can use the same logic
+ * but have different task dependencies.
+ */
+function build() {
+
+    var sourcemapPath = SCRIPTS_PATH + '/' + OUTPUT_FILE + '.map';
+    logBuildMode();
+
+    return browserify({
+        paths: [path.join(__dirname, 'src')],
+        entries: ENTRY_FILE,
+        debug: true,
+        transform: [
+            [
+                babelify, {
+                presets: ["es2015"]
+            }
+            ]
+        ]
+    })
+        .transform(babelify)
+        .bundle().on('error', function(error) {
+            gutil.log(gutil.colors.red('[Build Error]', error.message));
+            this.emit('end');
+        })
+        .pipe(gulpif(!isProduction(), exorcist(sourcemapPath)))
+        .pipe(source(OUTPUT_FILE))
+        .pipe(buffer())
+        .pipe(gulpif(isProduction(), uglify()))
+        .pipe(gulp.dest(SCRIPTS_PATH));
+
+}
+
+/**
+ * Starts the Browsersync server.
+ * Watches for file changes in the 'src' folder.
+ */
+function serve() {
+
+    var options = {
+        server: {
+            baseDir: BUILD_PATH
+        },
+        open: false // Change it to true if you wish to allow Browsersync to open a browser window.
+    };
+
+    browserSync(options);
+
+    // Watches for changes in files inside the './src' folder.
+    gulp.watch(SOURCE_PATH + '/**/*.js', ['watch-js']);
+
+    // Watches for changes in files inside the './static' folder. Also sets 'keepFiles' to true (see cleanBuild()).
+    gulp.watch(STATIC_PATH + '/**/*', ['watch-static']).on('change', function() {
+        keepFiles = true;
+    });
+
+}
+
+
+gulp.task('cleanBuild', cleanBuild);
+gulp.task('copyStatic', ['cleanBuild'], copyStatic);
+gulp.task('build', ['copyStatic'], build);
+gulp.task('fastBuild', build);
+gulp.task('serve', ['build'], serve);
+gulp.task('watch-js', ['fastBuild'], browserSync.reload); // Rebuilds and reloads the project when executed.
+gulp.task('watch-static', ['copyStatic'], browserSync.reload);
+
+/**
+ * The tasks are executed in the following order:
+ * 'cleanBuild' -> 'copyStatic' -> 'build' -> 'serve'
+ *
+ * Read more about task dependencies in Gulp:
+ * https://medium.com/@dave_lunny/task-dependencies-in-gulp-b885c1ab48f0
+ */
+gulp.task('default', ['serve']);

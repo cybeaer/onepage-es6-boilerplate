0d085802fab520a5be9507eda81e73da8c28222b
Michael Attenberger <attenberger@gmail.com>
2017-11-01 11:12:28 +0100

    cleaned up package.json & example output

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function test() {
-    console.log('script runs');
+  console.log('script runs');
+  let app = document.getElementById('app');
+  if(!app)
+    document.write('cant get app div!');
+  else
+    app.innerHTML = '<h1>Hello World</h1>';
 }
 
 test();
